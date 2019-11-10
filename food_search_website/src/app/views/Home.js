2553447dfef6dfa7a7c6766b80b7654c79b4393f
Caitlin Loh <celoh@uci.edu>
2019-11-10 14:20:45 -0800

    HTML for home page

--- food_search_website/src/app/views/Home.js
+++ food_search_website/src/app/views/Home.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import { Route, Switch, Router } from 'react-router-dom';
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 function Home(){
-    return (<h1>Home</h1>)
+
+    function handleInputChange(e) {
+
+    }
+
+    function handleSubmit(e) {
+
+    }
+
+    return (
+        <div>
+            <h1>Food Search</h1>
+            <Form>
+                <Form.Label>Enter food:</Form.Label>
+                <p></p>
+                <Form.Control name="food" onChange={handleInputChange}></Form.Control>
+                <p></p>
+                <Button onClick={handleSubmit}>Submit</Button>
+            </Form>
+        </div>
+    )
 }
 
 export default Home;
